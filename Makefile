cf999976973fc0c519d852ac4fdb93940954e9ee
Patrick LeBoutillier <patrick.leboutillier@gmail.com>
2021-06-02 13:10:26 -0400

    ok

--- Makefile
+++ Makefile
@@ -1,19 +1,18 @@
-modules: minihdl modules/*.yaml
-	cd modules && ./gen.sh
-	g++ -c -Iminihdl -Imodules modules/modules.cpp -o modules/modules.o
-	for f in modules/*.yaml ; do name=$$(basename $$f .yaml) ; g++ -c -Iminihdl -Imodules modules/$$name.cpp -o modules/$$name.o || exit 1 ; done
-	for f in modules/*.yaml ; do name=$$(basename $$f .yaml) ; g++ -o modules/$$name.test modules/$$name.o modules/modules.o minihdl/minihdl.o || exit 1 ; done
-	for f in modules/*.yaml ; do name=$$(basename $$f .yaml) ; modules/$$name.test > sim/$$name.sim || exit 1; done
+.PHONY: modules
+modules: 
+	make -C modules modules
 
 
 .PHONY: minihdl
 minihdl: 
-	make -C minihdl
+	make -C minihdl minihdl.o
+
 
 clean:
 	make -C minihdl clean
-	rm -f modules/*.cpp modules/*.h modules/*.o modules/*.test sim/*.sim t/*.t t/*.tdata
+	make -C modules clean
 
 
 test: modules
+	make -C modules sim test
 	prove -Iminihdl -It
--- modules/gen.sh
+++ modules/gen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat <<H > modules.h
+cat <<H > out/modules.h
 #ifndef MODULES_H
 #define MODULES_H
 
@@ -8,7 +8,7 @@ cat <<H > modules.h
 
 H
 
-cat <<H > modules.cpp
+cat <<H > out/modules.cpp
 #include "modules.h"
 
 H
@@ -16,18 +16,18 @@ H
 
 for f in *.yaml ; do
 	name=$(basename $f .yaml)
-	yq e .header $f >> modules.h
-	yq e .source $f >> modules.cpp
+	yq e .header $f >> out/modules.h
+	yq e .source $f >> out/modules.cpp
 	rank=$(printf "%02d" "$(yq e .rank $f)")
 	yq e .testperl $f > ../t/${rank}_$name.t
 	yq e .testdata $f > ../t/$name.tdata
 
-	echo -e "#include \"minihdl.h\"\n#include \"modules.h\"\n\nint main(){\ninit() ; \n" > $name.cpp
-	yq e .test $f >> $name.cpp
-	echo -e "done() ;\n}\n" >> $name.cpp
+	#echo -e "#include \"minihdl.h\"\n#include \"modules.h\"\n\nint main(){\ninit() ; \n" > out/$name.cpp
+	#yq e .test $f >> out/$name.cpp
+	#echo -e "done() ;\n}\n" >> out/$name.cpp
 done
 
-cat <<H >> modules.h
+cat <<H >> out/modules.h
 
 #endif 
 H
